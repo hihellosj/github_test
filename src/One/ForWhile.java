3399da2ddc09e835544708ef565506c1df4c91d5
Soojeong Lee <sapphire3509@gmail.com>
2023-06-09 08:21:22 +0900

    this is commit message

---
+++ src/One/ForWhile.java
@@ -0,0 +1,13 @@
+package One;
+
+public class ForWhile {
+
+	public static void main(String[] args) {
+		for(int i=1; i<=6; i++)
+			for(int j=1;j<=6;i++)
+				if(i+j==6)
+					System.out.println(i+"+"+j+"="+(i+j));
+
+	}
+
+}
